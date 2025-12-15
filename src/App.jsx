3f98ba303085f9e95209cd70d7821c380bcd048e
Delvitanaa <delvitaartika@gmail.com>
2025-12-15 22:35:01 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css'
+import CRUDOperationsDemo from './CRUDOperationsDemo'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Demo CRUD dengan React</h1>
+      <CRUDOperationsDemo />
+    </div>
+  )
+}
+
+export default App
